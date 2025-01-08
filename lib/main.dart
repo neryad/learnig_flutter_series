860032ba536fa0a4c99a93193d53b4b19df70c8d
neryad <dayern.gomez@hotmail.com>
2025-01-08 10:58:01 -0400

    implemtacion de drawer y listTile

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,69 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Widget _selectedWidget = const Center(child: Text('Selecciona un widget'));
+
+  void _updateWidget(Widget widget) {
+    setState(() {
+      _selectedWidget = widget;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hola mundo!'),
+        appBar: AppBar(
+          title: const Text('Widgets Básicos en Flutter'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print('hola desde el appbar');
+                },
+                icon: const Icon(Icons.settings))
+          ],
+        ),
+        body: _selectedWidget,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(child: Text('Drawer Header')),
+              ListTile(
+                title: const Text('Texto'),
+                subtitle: const Text('Ejemplo del texto'),
+                leading: const Icon(Icons.text_fields),
+                trailing: const Icon(Icons.arrow_forward),
+                onTap: () {
+                  _updateWidget(const Text(
+                    'Hola soy el texto',
+                    style: TextStyle(fontSize: 24, color: Colors.blue),
+                  ));
+                  //Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Texto'),
+                subtitle: const Text('Ejemplo del texto'),
+                leading: const Icon(Icons.text_fields),
+                trailing: const Icon(Icons.arrow_forward),
+                onTap: () {
+                  _updateWidget(const Text(
+                    'Hola soy el texto2',
+                    style: TextStyle(fontSize: 24, color: Colors.red),
+                  ));
+                  //Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
